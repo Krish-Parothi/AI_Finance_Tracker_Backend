c1af26e433eb5f02a87a05f8533af70e555d3eb4
Krish-Parothi <krrishparothi10@gmail.com>
2025-12-10 20:30:07 +0530

    allow_origin url

--- app/utils/auth_dependency.py
+++ app/utils/auth_dependency.py
@@ -9,9 +9,17 @@ from app.utils.jwt_handler import decode_token
 #     payload = decode_token(token)
 #     return payload["user_id"]
 
-async def auth_user(req: Request):
-    token = req.cookies.get("access_token")
-    if not token:
-        raise HTTPException(401, "Not authenticated")
+# async def auth_user(req: Request):
+#     token = req.cookies.get("access_token")
+#     if not token:
+#         raise HTTPException(401, "Not authenticated")
+#     payload = decode_token(token)
+#     return payload["user_id"]
+
+async def auth_user(authorization: str = Header(None)):
+    if not authorization:
+        raise HTTPException(401, "No authorization header")
+
+    token = authorization.split(" ")[1]
     payload = decode_token(token)
     return payload["user_id"]
