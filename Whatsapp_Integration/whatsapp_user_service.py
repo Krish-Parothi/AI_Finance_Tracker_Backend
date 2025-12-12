2e48a46dd3b3be53736d01f83723a4af36fe887e
Krish-Parothi <krrishparothi10@gmail.com>
2025-12-12 09:03:21 +0530

    whatsapp integration

--- Whatsapp_Integration/whatsapp_user_service.py
+++ Whatsapp_Integration/whatsapp_user_service.py
@@ -32,9 +32,9 @@ def signup_user(phone: str, email: str, password: str):
     return user_doc
 
 
-def login_user(phone: str, password: str):
+def login_user(email: str, password: str):
     # Find user by phone or email
-    user = users.find_one({"phone": phone})
+    user = users.find_one({"email": email})
 
     if not user:
         return None
@@ -43,3 +43,32 @@ def login_user(phone: str, password: str):
         return None
 
     return user
+
+
+
+# # whatsapp_user_service.py
+# from Whatsapp_Integration.db import db
+# from werkzeug.security import generate_password_hash, check_password_hash
+# from datetime import datetime
+
+# users = db["users"]
+
+# def signup_user(phone: str, username: str, password: str):
+#     if users.find_one({"phone": phone}):
+#         return None  # Already exists
+#     hashed_pw = generate_password_hash(password)
+#     user_doc = {
+#         "phone": phone,
+#         "username": username,
+#         "password": hashed_pw,
+#         "public_code": f"USER{int(datetime.utcnow().timestamp())}",
+#         "created_at": datetime.utcnow().isoformat()
+#     }
+#     users.insert_one(user_doc)
+#     return user_doc
+
+# def login_user(phone: str, password: str):
+#     user = users.find_one({"phone": phone})
+#     if not user or not check_password_hash(user["password"], password):
+#         return None
+#     return user
